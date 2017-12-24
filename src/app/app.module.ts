1312813a66286e7ab296fd5613d405af51afade3
Sergio Gregorutti <sergiogregorutti@gmail.com>
2017-12-24 17:10:46 -0300

    Add container to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from "@angular/forms";
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products/products-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from "./shared/star.component";
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
